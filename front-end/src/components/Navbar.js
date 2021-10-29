05da886aabd95cf63bc79133f236939314c62f54
AlanPL <apyolt@gmail.com>
2021-10-29 00:20:01 -0500

    add register screen

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -4,16 +4,16 @@ import redstone from '../images/redstone.png';
 
 export default function Navbar() {
 	return (
-		<div className='py-2 pr-8 pl-5 bg-gray-dark bg-opacity-70 fixed top-0 w-full flex shadow-lg'>
+		<div className='py-1 pr-8 pl-5 bg-gray-dark bg-opacity-70 fixed top-0 w-full flex shadow-lg'>
 			<div className='flex  mr-3'>
 				<img src={redstone} className='m-auto w-10'  />
 			</div>
 			<h3 className='font-bold text-xl my-auto'>Network Administrator</h3>
 			<div className='m-auto mr-0 space-x-5 text-lg '>
-				<Link to="/register">
+				<Link to="/register" className=' p-3'>
 					Register
 				</Link>
-				<Link to="/login">
+				<Link to="/login" className=' p-3'>
 					Login
 				</Link>
 			</div>
--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Button from '../components/Button'
+import Input from '../components/Input'
+import Title from '../components/Title'
 
 export default function Register() {
 	return (
-		<div>
-			Register	
+		<div className='bg-opacity-70 w-1/3 m-auto h-screen py-32'>
+			<div className='bg-gray-dark bg-opacity-70 h-full rounded-2xl text-center p-10 px-20'>
+				<Title>
+					Register
+				</Title>
+				<div className='space-y-10 my-16'>
+					<Input placeholder={'username'} label={'Username'} />
+					<Input placeholder={'email'} label={'E-mail'} />
+					<Input placeholder={'password'} label={'Password'} />
+				</div>
+				<Button label={'Register'} />
+			</div>
 		</div>
 	)
 }
